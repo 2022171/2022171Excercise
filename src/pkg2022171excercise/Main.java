0756a4e5e6e58dd8684b76092a2e7a5926e46c39
2022171 <146198931+2022171@users.noreply.github.com>
2023-09-29 23:33:51 +0100

    Initial commit

---
+++ src/pkg2022171excercise/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2022171excercise;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danie
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           Scanner myKB = new Scanner(System.in);
+        
+        String name;
+        
+        System.out.println("What is your name");
+        
+        name = myKB.nextLine();
+        
+    }
+    
+    
+    
+}
